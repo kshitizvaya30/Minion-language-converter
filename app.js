3ec4ebf206b53e2b06e4bddb02279f3b6e52a401
kshitizvaya <k.vaya301201@gmail.com>
2020-12-19 22:38:12 +0530

    css updated

--- app.js
+++ app.js
@@ -2,11 +2,33 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+//Server URL
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+btnTranslate.addEventListener("click", clickEventHandler);
+
+function getTranslationURL(text) {
+    return serverURL + "?text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("Failed to Fetch", error);
+    alert("Something wrong with Server! Try Again after some time");
+}
 
 
-function clickEventHandler(){
-    outputDiv.innerText = "zkvbuiosdgvibviehvip"  +  txtInput.value;
+//Calling Server For Processing
+function clickEventHandler() {
+
+    //Taking Input
+    var inputText = txtInput.value;
+
+    //Calling server for Processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText
+        })
+        .catch(errorHandler);
 };
-
- 
-btnTranslate.addEventListener("click" ,clickEventHandler);
