4c43a7536af2de054aa72774fd714809dd5addb7
kshitizvaya <k.vaya301201@gmail.com>
2020-12-19 19:04:28 +0530

    input and output done

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput)
 
-btnTranslate.addEventListener("click" , function clickEventHandler() {
-    console.log("clicked!");
-    console.log("input",txtInput.value);
-});
 
+function clickEventHandler(){
+    outputDiv.innerText = "zkvbuiosdgvibviehvip"  +  txtInput.value;
+};
+
+ 
+btnTranslate.addEventListener("click" ,clickEventHandler);
